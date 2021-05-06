a4df9a4c7f28a75f53a78f57507d7f0a0ffcef42
haiq70 <jkwasniak00@gmail.com>
2021-05-06 18:44:05 +0200

    set up the script and initialised adjacency matrices

---
+++ dijkstra.py
@@ -0,0 +1,15 @@
+# Dijkstra's shortest path algorithm - visualisation
+# // haiq70
+
+# intialising adjacency matrices
+# existence of connections between vertices
+vertices = [[0, 1, 1, 0],
+            [1, 0, 1, 0],
+            [1, 1, 0, 1],
+            [0, 0, 1, 0]]
+
+# weighted nodes representing paths between vertices
+nodes = [[0, 1, 2, 0],
+         [1, 0, 4, 0],
+         [2, 4, 0, 3],
+         [0, 0, 3, 0]]
